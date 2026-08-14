b2594ea475fc4afa85b4d7f42c607ca94739e8b8
BOULAHYA MARWAN <102596690+boulahya01@users.noreply.github.com>
2026-08-14 19:30:32 +0100

    feat: export public receipt API

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export { OpenReceiptError } from "./errors.js";
+export type { OpenReceiptErrorCode } from "./errors.js";
+export { ReceiptBuilder, receipt } from "./receipt.js";
+export type {
+  Alignment,
+  ReceiptCutNode,
+  ReceiptDividerNode,
+  ReceiptDocument,
+  ReceiptFeedNode,
+  ReceiptItemNode,
+  ReceiptNode,
+  ReceiptTextNode,
+  ReceiptTotalNode,
+} from "./types.js";
